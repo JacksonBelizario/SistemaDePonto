f40a9065c39ac48a52ad8af4e69a95fca5dd5d81
Jackson Belizario <jacksonluizzb@gmail.com>
2020-03-12 16:59:41 -0400

    Commit inicial

---
+++ ClientApp/src/components/NavMenu.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        title: {
+            flexGrow: 1,
+        },
+    }),
+);
+
+export default function NavMenu() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Sistema de Ponto
+                    </Typography>
+                    <Button component={RouterLink} to="/" color="inherit">Home</Button>
+                    <Button component={RouterLink} to="/counter" color="inherit">Counter</Button>
+                    <Button component={RouterLink} to="/reports" color="inherit">Relatorios</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
