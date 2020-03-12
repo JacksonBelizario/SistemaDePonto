c4b7bf47efcc06b2c7b82dd77c9dba9a7cc99217
Jackson Belizario <jacksonluizzb@gmail.com>
2020-03-12 18:30:44 -0400

    Seletores de Data

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,16 +1,22 @@
 import * as React from 'react';
 import { Route } from 'react-router';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
 import Layout from './components/Layout';
 import Home from './views/Home';
 import Counter from './components/Counter';
 import Reports from './components/Reports';
-
+import moment from "moment";
+import "moment/locale/pt-br";
 import './custom.css'
+// moment.locale("pt-br");
 
 export default () => (
-    <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/reports/:startDateIndex?' component={Reports} />
-    </Layout>
+    <MuiPickersUtilsProvider utils={MomentUtils} libInstance={moment}>
+        <Layout>
+            <Route exact path='/' component={Home} />
+            <Route path='/counter' component={Counter} />
+            <Route path='/reports/:startDateIndex?' component={Reports} />
+        </Layout>
+    </MuiPickersUtilsProvider>
 );
--- ClientApp/src/views/Home.tsx
+++ ClientApp/src/views/Home.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
@@ -14,8 +14,25 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+
+import {
+    KeyboardTimePicker,
+    KeyboardDatePicker
+} from '@material-ui/pickers';
+import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
+
 import FolderIcon from '@material-ui/icons/Folder';
 import DeleteIcon from '@material-ui/icons/Delete';
+import AddCircleOutlineRoundedIcon from '@material-ui/icons/AddCircleOutlineRounded';
+
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -42,7 +59,119 @@ function generate(element: React.ReactElement) {
 
 const Home = () => {
     const classes = useStyles();
-    const [secondary, setSecondary] = React.useState(false);
+    const [secondary, setSecondary] = React.useState<boolean>(false);
+    const [open, setOpen] = React.useState<boolean>(false);
+    const [selectedDate, setSelectedDate] = React.useState<Date | MaterialUiPickersDate | null>(null);
+    const [entrada, setEntrada] = React.useState<Date | MaterialUiPickersDate | null>(null);
+    const [saidaAlmoco, setSaidaAlmoco] = React.useState<Date | MaterialUiPickersDate | null>(null);
+    const [entradaAlmoco, setEntradaAlmoco] = React.useState<Date | MaterialUiPickersDate | null>(null);
+    const [saida, setSaida] = React.useState<Date | MaterialUiPickersDate | null>(null);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const renderDialog = () => (
+        <div>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Bater ponto</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Informe os horarios de seu ponto
+                    </DialogContentText>
+                    <Grid container justify="space-between" spacing={2}>
+                         <Grid item xs={12}>
+                            <KeyboardDatePicker
+                                disableToolbar
+                                variant="inline"
+                                format="DD/MM/YYYY"
+                                margin="normal"
+                                id="date-picker-inline"
+                                label="Data"
+                                value={selectedDate}
+                                onChange={setSelectedDate}
+                                KeyboardButtonProps={{
+                                    'aria-label': 'change date',
+                                }}
+                                />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <KeyboardTimePicker
+                                disableToolbar
+                                variant="inline"
+                                margin="normal"
+                                id="time-picker"
+                                label="Entrada"
+                                ampm={false}
+                                value={entrada}
+                                onChange={setEntrada}
+                                KeyboardButtonProps={{
+                                    'aria-label': 'change time',
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <KeyboardTimePicker
+                                disableToolbar
+                                variant="inline"
+                                margin="normal"
+                                id="time-picker"
+                                label="Saida almoco"
+                                ampm={false}
+                                value={saidaAlmoco}
+                                onChange={setSaidaAlmoco}
+                                KeyboardButtonProps={{
+                                    'aria-label': 'change time',
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <KeyboardTimePicker
+                                disableToolbar
+                                variant="inline"
+                                margin="normal"
+                                id="time-picker"
+                                label="Entrada almoco"
+                                ampm={false}
+                                value={entradaAlmoco}
+                                onChange={setEntradaAlmoco}
+                                KeyboardButtonProps={{
+                                    'aria-label': 'change time',
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <KeyboardTimePicker
+                                disableToolbar
+                                variant="inline"
+                                margin="normal"
+                                id="time-picker"
+                                label="Saida"
+                                ampm={false}
+                                value={saida}
+                                onChange={setSaida}
+                                KeyboardButtonProps={{
+                                    'aria-label': 'change time',
+                                }}
+                            />
+                        </Grid>
+                    </Grid>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleClose} color="primary">
+                        Subscribe
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
 
     return (
         <div className={classes.root}>
@@ -62,6 +191,9 @@ const Home = () => {
                 <Grid item xs={12}>
                     <Typography variant="h6" className={classes.title}>
                         Nome do funcionario
+                        <IconButton edge="end" color="primary" onClick={handleClickOpen}>
+                            <AddCircleOutlineRoundedIcon fontSize="large" />
+                        </IconButton>
                     </Typography>
                     <div className={classes.demo}>
                         <List>
@@ -87,6 +219,7 @@ const Home = () => {
                     </div>
                 </Grid>
             </Grid>
+            {renderDialog()}
         </div>
     );
 }
