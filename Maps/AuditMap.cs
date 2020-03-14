43ba24ffc78aa19048dff49f0382674f42b6f17a
Jackson Belizario <jacksonluizzb@gmail.com>
2020-03-14 17:33:43 -0400

    Audit Model

---
+++ Maps/AuditMap.cs
@@ -0,0 +1,23 @@
+﻿using SistemaDePonto.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SistemaDePonto.Maps
+{
+    public class AuditMap
+    {
+        public AuditMap(EntityTypeBuilder<Audit> entityBuilder)
+        {
+            entityBuilder.HasKey(x => x.Id);
+            entityBuilder.ToTable("audit");
+
+            entityBuilder.Property(x => x.Id).HasColumnName("id");
+            entityBuilder.Property(x => x.UserId).HasColumnName("user_id");
+            entityBuilder.Property(x => x.TableName).HasColumnName("table_name");
+            entityBuilder.Property(x => x.DateTime).HasColumnName("date_time");
+            entityBuilder.Property(x => x.KeyValues).HasColumnName("key_values");
+            entityBuilder.Property(x => x.OldValues).HasColumnName("old_values");
+            entityBuilder.Property(x => x.NewValues).HasColumnName("new_values");
+        }
+    }
+}
---
+++ Models/Audit.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaDePonto.Models
+{
+    public class Audit
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public string TableName { get; set; }
+        public DateTime DateTime { get; set; }
+        public string KeyValues { get; set; }
+        public string OldValues { get; set; }
+        public string NewValues { get; set; }
+        public string IPAddress { get; set; }
+    }
+
+    public class AuditEntry
+    {
+        public AuditEntry(EntityEntry entry)
+        {
+            Entry = entry;
+        }
+
+        public EntityEntry Entry { get; }
+        public string TableName { get; set; }
+        public string IPAddress { get; set; }
+        public Dictionary<string, object> KeyValues { get; } = new Dictionary<string, object>();
+        public Dictionary<string, object> OldValues { get; } = new Dictionary<string, object>();
+        public Dictionary<string, object> NewValues { get; } = new Dictionary<string, object>();
+        public List<PropertyEntry> TemporaryProperties { get; } = new List<PropertyEntry>();
+
+        public bool HasTemporaryProperties => TemporaryProperties.Any();
+
+        public Audit ToAudit()
+        {
+            var audit = new Audit();
+
+            audit.TableName = TableName;
+            audit.IPAddress = IPAddress;
+            audit.DateTime = DateTime.UtcNow;
+            audit.KeyValues = JsonConvert.SerializeObject(KeyValues);
+            audit.OldValues = OldValues.Count == 0 ? null : JsonConvert.SerializeObject(OldValues);
+            audit.NewValues = NewValues.Count == 0 ? null : JsonConvert.SerializeObject(NewValues);
+
+            return audit;
+        }
+    }
+}
